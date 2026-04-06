a25df3442b018967c79dc549bf3fd555c6005e1b
Anshika Arjariya <anshikaarjariya07@gmail.com>
2026-04-06 21:10:12 +0530

    Add files via upload

---
+++ Questions/q65.c
@@ -0,0 +1,60 @@
+/*
+Problem: Using DFS and parent tracking, detect if undirected graph has a cycle.
+
+Output:
+- YES or NO*/
+#include <stdio.h>
+#include <stdbool.h>
+
+#define MAX 100
+
+int graph[MAX][MAX];
+bool visited[MAX];
+int n; // number of vertices
+
+bool dfs(int node, int parent) {
+    visited[node] = true;
+
+    for (int i = 0; i < n; i++) {
+        if (graph[node][i]) { // edge exists
+            if (!visited[i]) {
+                if (dfs(i, node))
+                    return true;
+            }
+            else if (i != parent) {
+                return true; // cycle detected
+            }
+        }
+    }
+    return false;
+}
+
+bool hasCycle() {
+    for (int i = 0; i < n; i++) {
+        if (!visited[i]) {
+            if (dfs(i, -1))
+                return true;
+        }
+    }
+    return false;
+}
+
+int main() {
+    printf("Enter number of vertices: ");
+    scanf("%d", &n);
+
+    printf("Enter adjacency matrix:\n");
+    for (int i = 0; i < n; i++) {
+        visited[i] = false;
+        for (int j = 0; j < n; j++) {
+            scanf("%d", &graph[i][j]);
+        }
+    }
+
+    if (hasCycle())
+        printf("YES\n");
+    else
+        printf("NO\n");
+
+    return 0;
+}
