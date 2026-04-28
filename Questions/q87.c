2b467b3201ba57d9d091f0ec8ac001998b6d7d0e
Anshika Arjariya <anshikaarjariya07@gmail.com>
2026-04-28 23:38:57 +0530

    Add files via upload

---
+++ Questions/q87.c
@@ -0,0 +1,44 @@
+/*
+Problem: Implement Binary Search Iterative - Implement the algorithm.
+
+Input:
+- First line: integer n
+- Second line: n space-separated integers
+
+Output:
+- Print the sorted array or search result
+
+Example:
+Input:
+5
+64 34 25 12 22
+
+Output:
+12 22 25 34 64*/
+#include <stdio.h>
+#include <stdlib.h>
+
+int compare(const void *a, const void *b) {
+    return (*(int*)a - *(int*)b);
+}
+
+int main() {
+    int n;
+    scanf("%d", &n);
+
+    int arr[n];
+
+    for (int i = 0; i < n; i++) {
+        scanf("%d", &arr[i]);
+    }
+
+    qsort(arr, n, sizeof(int), compare);
+
+    for (int i = 0; i < n; i++) {
+        printf("%d", arr[i]);
+        if (i != n - 1)
+            printf(" ");
+    }
+
+    return 0;
+}
